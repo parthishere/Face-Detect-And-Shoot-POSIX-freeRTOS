793966b4f4fcacfa3910355f42e148ced6ac19fa
parthishere <parthishere1234@gmail.com>
2024-04-27 01:01:05 -0600

    working

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ CC=g++
 CDEFS=
 CFLAGS= -O0 -g $(INCLUDE_DIRS) $(CDEFS)
 LDFLAGS= -Wl,-Map,output.map
-LIBS= -L/usr/lib -lopencv_core -lopencv_flann -lopencv_video -lrt
+LIBS= -L/usr/lib -lopencv_core -lopencv_flann -lopencv_video -lrt -lpthread
 
 HFILES= 
 CFILES= facedetect.cpp
@@ -13,13 +13,20 @@ CFILES= facedetect.cpp
 SRCS= ${HFILES} ${CFILES}
 OBJS= ${CFILES:.cpp=.o}
 
-all:	facedetect 
+# Name of binary
+MAIN = facedetect
+
+run: $(MAIN)
+	@sudo ./$(MAIN) $(ARGS)
+
+all: clean $(MAIN) run
+	@echo Program Compiled
 
 clean:
 	-rm -f *.o *.d
-	-rm -f facedetect 
+	-rm -f $(MAIN) 
 
-facedetect: facedetect.o
+$(MAIN): facedetect.o
 	$(CC) $(LDFLAGS) $(CFLAGS) -o $@ $@.o `pkg-config --libs opencv4` $(LIBS)
 
 %.o: %.cpp
--- facedetect.cpp
+++ facedetect.cpp
@@ -1,4 +1,3 @@
-#define _GNU_SOURCE
 #include "opencv2/objdetect.hpp"
 #include "opencv2/videoio.hpp"
 #include "opencv2/highgui.hpp"
@@ -15,17 +14,15 @@
 #include <sys/sysinfo.h>
 #include <stdbool.h>
 #include <semaphore.h>
+#include <sys/time.h>
 
 #define NUMBER_OF_TASKS 3
 
 
-using namespace std;
-using namespace cv;
-
 /** Global variables */
-String faceCascadePath;
-CascadeClassifier faceCascade;
-
+cv::String faceCascadePath;
+cv::CascadeClassifier faceCascade;
+double overall_start_time, overall_stop_time;
 
 typedef struct
 {
@@ -46,6 +43,7 @@ typedef struct
     int target_cpu;
 } RmTask_t;
 
+sem_t semaphore_10ms, semaphore_20ms;
 
 double read_time(double *var)
 {
@@ -64,40 +62,7 @@ double read_time(double *var)
 
 
 
-void *thread10ms(void *args)
-{
-    RmTask_t *task_parameters  = (RmTask_t *)args;
-    
-    struct sched_param schedule_param;
-    int policy;
-    double execution_complete_time_for_a_loop;
-    double execution_start_time_for_a_loop;
-    bool preempt = false;
-
-    while (!abortTest_10)
-    {
-        // printf("thread 10 running | time stamp(arrival) %lf msec \n", (execution_start_time_for_a_loop - overall_start_time));
-        // if(preempt && sem_trywait(&semaphore_10ms)){
-        //     printf("Thread 10 preempted!\n");
-        // }
-        read_time(&execution_start_time_for_a_loop);
-        sem_wait(&semaphore_10ms);
-   
-        printf("fib 10 started\n");
-        fib_test(task_parameters->burst_time);
-        // fib_test(10);
-        pthread_getschedparam(pthread_self(), &policy, &schedule_param);
-        read_time(&execution_complete_time_for_a_loop);
-        
-        printf("Thread10 | priority = %d | time stamp(arrival) %lf msec | CPU burst time : %lf \n", schedule_param.sched_priority, (execution_start_time_for_a_loop - overall_start_time), (execution_complete_time_for_a_loop - execution_start_time_for_a_loop));
-        preempt = true;
-    }
-
-    return NULL;
-}
-
-
-void detectFaceOpenCVHaar(CascadeClassifier faceCascade, Mat &frameOpenCVHaar, int inHeight=300, int inWidth=0)
+void detectFaceOpenCVHaar(cv::CascadeClassifier faceCascade, cv::Mat &frameOpenCVHaar, int inHeight=300, int inWidth=0)
 {
     int frameHeight = frameOpenCVHaar.rows;
     int frameWidth = frameOpenCVHaar.cols;
@@ -107,11 +72,11 @@ void detectFaceOpenCVHaar(CascadeClassifier faceCascade, Mat &frameOpenCVHaar, i
     float scaleHeight = frameHeight / (float)inHeight;
     float scaleWidth = frameWidth / (float)inWidth;
 
-    Mat frameOpenCVHaarSmall, frameGray;
-    resize(frameOpenCVHaar, frameOpenCVHaarSmall, Size(inWidth, inHeight));
-    cvtColor(frameOpenCVHaarSmall, frameGray, COLOR_BGR2GRAY);
+    cv::Mat frameOpenCVHaarSmall, frameGray;
+    cv::resize(frameOpenCVHaar, frameOpenCVHaarSmall, cv::Size(inWidth, inHeight));
+    cv::cvtColor(frameOpenCVHaarSmall, frameGray, cv::COLOR_BGR2GRAY);
 
-    std::vector<Rect> faces;
+    std::vector<cv::Rect> faces;
     faceCascade.detectMultiScale(frameGray, faces);
 
     for ( size_t i = 0; i < faces.size(); i++ )
@@ -120,7 +85,130 @@ void detectFaceOpenCVHaar(CascadeClassifier faceCascade, Mat &frameOpenCVHaar, i
       int y1 = (int)(faces[i].y * scaleHeight);
       int x2 = (int)((faces[i].x + faces[i].width) * scaleWidth);
       int y2 = (int)((faces[i].y + faces[i].height) * scaleHeight);
-      rectangle(frameOpenCVHaar, Point(x1, y1), Point(x2, y2), Scalar(0,255,0), (int)(frameHeight/150.0), 4);
+      rectangle(frameOpenCVHaar, cv::Point(x1, y1), cv::Point(x2, y2), cv::Scalar(0,255,0), (int)(frameHeight/150.0), 4);
+    }
+}
+
+
+void *FaceDetectService(void *args)
+{
+    RmTask_t *task_parameters  = (RmTask_t *)args;
+    
+   faceCascadePath = "./haarcascade_frontalface_default.xml";
+    if(!faceCascade.load(faceCascadePath))
+    {
+        printf("--(!)Error loading face cascade\n");
+        return NULL;
+    }
+
+    cv::VideoCapture source;
+    
+    source.open(0, cv::CAP_V4L);
+
+    cv::Mat frame;
+
+    double tt_opencvHaar = 0;
+    double fpsOpencvHaar = 0;
+
+    while (true)
+    {
+        source >> frame;
+        if (frame.empty())
+            break;
+
+        double t = cv::getTickCount();
+        detectFaceOpenCVHaar(faceCascade, frame);
+        tt_opencvHaar = ((double)cv::getTickCount() - t)/cv::getTickFrequency();
+        fpsOpencvHaar = 1/tt_opencvHaar;
+
+        cv::putText(frame, cv::format("OpenCV HAAR ; FPS = %.2f",fpsOpencvHaar), cv::Point(10, 50), cv::FONT_HERSHEY_SIMPLEX, 1.3, cv::Scalar(0, 0, 255), 4);
+
+        cv::imshow("OpenCV - HAAR Face Detection", frame);
+
+        int k = cv::waitKey(5);
+        if(k == 27)
+        {
+            cv::destroyAllWindows();
+            break;
+        }
+    }
+    return NULL;
+}
+
+
+void *ServoActuatorService(void *args)
+{
+    RmTask_t *task_parameters  = (RmTask_t *)args;
+    
+    struct sched_param schedule_param;
+    int policy;
+    double execution_complete_time_for_a_loop;
+    double execution_start_time_for_a_loop;
+
+    while (1)
+    {
+        read_time(&execution_start_time_for_a_loop);
+        // sem_wait(&semaphore_10ms);
+   
+        printf("Actuation Service will execute\n");
+
+        pthread_getschedparam(pthread_self(), &policy, &schedule_param);
+        read_time(&execution_complete_time_for_a_loop);
+        
+        // printf("Thread10 | priority = %d | time stamp(arrival) %lf msec | CPU burst time : %lf \n", schedule_param.sched_priority, (execution_start_time_for_a_loop - overall_start_time), (execution_complete_time_for_a_loop - execution_start_time_for_a_loop));
+        sleep(1);
+    }
+
+    return NULL;
+}
+
+
+void *ServoShootService(void *args)
+{
+    RmTask_t *task_parameters  = (RmTask_t *)args;
+    
+    struct sched_param schedule_param;
+    int policy;
+    double execution_complete_time_for_a_loop;
+    double execution_start_time_for_a_loop;
+
+    while (1)
+    {
+        read_time(&execution_start_time_for_a_loop);
+        // sem_wait(&semaphore_10ms);
+   
+        printf("Shooting service will execute\n");
+
+        pthread_getschedparam(pthread_self(), &policy, &schedule_param);
+        read_time(&execution_complete_time_for_a_loop);
+        
+        // printf("Thread10 | priority = %d | time stamp(arrival) %lf msec | CPU burst time : %lf \n", schedule_param.sched_priority, (execution_start_time_for_a_loop - overall_start_time), (execution_complete_time_for_a_loop - execution_start_time_for_a_loop));
+        sleep(1);
+
+    }
+
+    return NULL;
+}
+
+
+
+void print_scheduler(void)
+{
+    int schedType;
+    schedType = sched_getscheduler(getpid());
+    switch (schedType)
+    {
+    case SCHED_FIFO:
+        printf("Pthread Policy is SCHED_FIFO\n");
+        break;
+    case SCHED_OTHER:
+        printf("Pthread Policy is SCHED_OTHER\n");
+        break;
+    case SCHED_RR:
+        printf("Pthread Policy is SCHED_OTHER\n");
+        break;
+    default:
+        printf("Pthread Policy is UNKNOWN\n");
     }
 }
 
@@ -130,39 +218,37 @@ int main( int argc, const char** argv )
 
 
     pthread_t threads[NUMBER_OF_TASKS];
-    int coreid = 1;
     cpu_set_t threadcpu;
 
-    CPU_SET(coreid, &threadcpu);
+    int rt_max_prio = sched_get_priority_max(SCHED_FIFO);
+    int rt_min_prio = sched_get_priority_min(SCHED_FIFO);
 
     RmTask_t tasks[NUMBER_OF_TASKS] = {
         {.period = 20,     // ms
          .burst_time = 10, // ms
-         .priority_param = {1},
+         .priority_param = {rt_max_prio},
+         .thread_handle = &FaceDetectService,
          .thread = threads[0],
-         .thread_handle = thread10ms,
          .thread_args = {0},
-         .return_Value = NULL,
-         .attribute = {0, 0},
          .target_cpu = 2},
 
         {.period = 50,
          .burst_time = 20,
-         .priority_param = {2},
+         .priority_param = {rt_max_prio - 1},
+         .thread_handle = &ServoActuatorService,
          .thread = threads[1],
-         .thread_handle = thread20ms,
-         .thread_args = {0},
-         .attribute = {0, 0},
+         .thread_args = {1},
          .target_cpu = 0},
 
-        // {.period = 100,
-        //  .burst_time = 10,
-        //  .priority_param = {3},
-        //  .thread = threads[2],
-        //  .thread_handle = thread_task,
-        //  .thread_args = {0},
-        //  .attribute = {0,0},
-        //  .target_cpu = 0}
+        {.period = 50,
+         .burst_time = 20,
+         .priority_param = {rt_max_prio - 2},
+         .thread_handle = &ServoShootService,
+         .thread = threads[2],
+         .thread_args = {2},
+         .target_cpu = 0}
+
+
     };
     //Initialize Semaphore
     sem_init(&semaphore_10ms, false, 1);
@@ -171,31 +257,28 @@ int main( int argc, const char** argv )
     pthread_attr_t attribute_flags_for_main; // for schedular type, priority
     struct sched_param main_priority_param;
 
-    cpu_set_t cpuset;
-    int target_cpu = 1; // core we want to run our process on
-
     printf("This system has %d processors configured and %d processors available.\n", get_nprocs_conf(), get_nprocs());
 
     printf("Before adjustments to scheduling policy:\n");
     print_scheduler();
 
-    CPU_ZERO(&cpuset); // clear all the cpus in cpuset
+    CPU_ZERO(&threadcpu); // clear all the cpus in cpuset
 
-    int rt_max_prio = sched_get_priority_max(SCHED_FIFO);
-    int rt_min_prio = sched_get_priority_min(SCHED_FIFO);
 
     main_priority_param.sched_priority = rt_max_prio;
     for (int i = 0; i < NUMBER_OF_TASKS; i++)
     {
-        tasks[i].priority_param.sched_priority = rt_max_prio - (2*i*i);
-
+        CPU_SET(tasks[i].target_cpu, &threadcpu);
+        
         // initialize attributes
         pthread_attr_init(&tasks[i].attribute);
-
+        
         pthread_attr_setinheritsched(&tasks[i].attribute, PTHREAD_EXPLICIT_SCHED);
         pthread_attr_setschedpolicy(&tasks[i].attribute, SCHED_FIFO);
         pthread_attr_setschedparam(&tasks[i].attribute, &tasks[i].priority_param);
         pthread_attr_setaffinity_np(&tasks[i].attribute, sizeof(cpu_set_t), &threadcpu);
+
+        CPU_ZERO(&threadcpu);
     }
 
     pthread_attr_init(&attribute_flags_for_main);
@@ -226,7 +309,7 @@ int main( int argc, const char** argv )
         // Second parameter is the flags that we want to give it to
         // third parameter is the routine we want to give
         // Fourth parameter is the value
-        printf("Setting thread %d to core %d\n", i, coreid);
+        printf("Setting thread %d to core %d\n", i, tasks[i].target_cpu);
         
         
 
@@ -238,42 +321,21 @@ int main( int argc, const char** argv )
         
     }
 
-    faceCascadePath = "./haarcascade_frontalface_default.xml";
-    if(!faceCascade.load(faceCascadePath))
+
+    printf("Test Conducted over %lf msec\n", (double)(overall_stop_time - overall_start_time));
+
+    for (int i = 0; i < NUMBER_OF_TASKS; i++)
     {
-        printf("--(!)Error loading face cascade\n");
-        return -1;
+        pthread_join(tasks[i].thread, &tasks[i].return_Value);
     }
 
-    VideoCapture source;
-    if (argc == 1)
-        source.open(0, CAP_V4L);
-    else
-        source.open(argv[1]);  Mat frame;
+    if (pthread_attr_destroy(&tasks[0].attribute) != 0)
+        perror("attr destroy");
+    if (pthread_attr_destroy(&tasks[1].attribute) != 0)
+        perror("attr destroy");
 
-    double tt_opencvHaar = 0;
-    double fpsOpencvHaar = 0;
+    sem_destroy(&semaphore_10ms);
+    sem_destroy(&semaphore_20ms);  
 
-    while (true)
-    {
-        source >> frame;
-        if (frame.empty())
-            break;
-
-        double t = cv::getTickCount();
-        detectFaceOpenCVHaar(faceCascade, frame);
-        tt_opencvHaar = ((double)cv::getTickCount() - t)/cv::getTickFrequency();
-        fpsOpencvHaar = 1/tt_opencvHaar;
-
-        putText(frame, format("OpenCV HAAR ; FPS = %.2f",fpsOpencvHaar), Point(10, 50), FONT_HERSHEY_SIMPLEX, 1.3, Scalar(0, 0, 255), 4);
-
-        imshow("OpenCV - HAAR Face Detection", frame);
-
-        int k = waitKey(5);
-        if(k == 27)
-        {
-            destroyAllWindows();
-            break;
-        }
-    }
+    
 }
